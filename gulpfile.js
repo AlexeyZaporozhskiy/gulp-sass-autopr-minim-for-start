792d8247f3d9398dfac0d6824e4a0fc40aba2ff4
AlexeyZaporozhskiy <45877718+AlexeyZaporozhskiy@users.noreply.github.com>
2019-03-19 22:45:37 +0200

    потихоньку настраиваю gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
+var gulp            = require('gulp');
+var browserSync     = require('browser-sync');
+var sass            = require('gulp-sass');
+const autoprefixer  = require('gulp-autoprefixer');
 
 
 // Static Server + watching scss/html files
@@ -9,16 +10,28 @@ gulp.task('serve', function() {
     browserSync.init({
         server: "./"
     });
-    gulp.watch("app/sass/*.sass", gulp.parallel('sass'));
+    gulp.watch("app/sass/*.sass", gulp.parallel('sass','autoprefixer'));
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
+
+
+
+
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/sass/*.sass")
+    return gulp.src('app/sass/*.+(sass|scss)')
         .pipe(sass())
+	    .pipe(autoprefixer(['last 15 versions']))
         .pipe(gulp.dest("production/css"))
         .pipe(browserSync.stream());
 });
+
+
+
+
+
+
+
 
 gulp.task('default', gulp.parallel('serve','sass'));
