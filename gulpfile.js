708497af4011960bdd93dbfc908524d3d458c9d5
AlexeyZaporozhskiy <45877718+AlexeyZaporozhskiy@users.noreply.github.com>
2019-03-19 22:00:03 +0200

    надумался обновить галп, треш-но вроде разобрался

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("app/sass/*.sass", gulp.parallel('sass'));
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/sass/*.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("production/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('serve','sass'));
